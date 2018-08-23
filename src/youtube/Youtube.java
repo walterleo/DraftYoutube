11f2acd3fa6cd9812cd6fd7efb677ed42fdf2106
Walter <Walter@DESKTOP-82TFBEC.NGV.local>
2018-08-23 13:45:09 +1000

    Draft1

---
+++ src/youtube/Youtube.java
@@ -0,0 +1,31 @@
+
+package youtube;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Youtube extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Youtube.fxml"));
+        
+        Scene scene = new Scene(root);
+         scene.getStylesheets().add("/CSS/sample.css");
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
